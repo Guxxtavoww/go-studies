b4507959948e9757739f49a5cb2148f36a6078ed
Guxxtavoww <gustavoaugustocar@hotmail.com>
2024-08-16 11:22:59 -0300

    Chore: init

--- first-go-project/main.go
+++ first-go-project/main.go
@@ -2,9 +2,23 @@ package main
 
 import "fmt"
 
-var boolean_value bool = false;
-var string_value string = "Hello, World!"
+type Rectangle struct {
+	width  float64
+	height float64
+}
+
+func (r *Rectangle) calculate_rectangle_area() float64 {
+	return r.width * r.height
+}
 
 func main() {
-	fmt.Println(string_value, boolean_value)
+	var rectangle *Rectangle = &Rectangle{
+		width: 10.25,
+		height: 3.24,
+	}
+
+	rectangle_area := rectangle.calculate_rectangle_area()
+
+	fmt.Println("Area", rectangle_area)
+
 }
