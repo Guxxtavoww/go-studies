636393bacac16a3e888c80d6a2dc3450ea4f3246
Guxxtavoww <gustavoaugustocar@hotmail.com>
2024-08-16 21:09:42 -0300

    Chore: init

--- intro/main.go
+++ intro/main.go
@@ -7,13 +7,13 @@ type Rectangle struct {
 	height float64
 }
 
-func (r *Rectangle) calculate_rectangle_area() float64 {
-	return r.width * r.height
+func (rectangle *Rectangle) calculate_rectangle_area() float64 {
+	return rectangle.width * rectangle.height
 }
 
 func main() {
 	var rectangle *Rectangle = &Rectangle{
-		width: 10.25,
+		width:  10.25,
 		height: 3.24,
 	}
 
