7a04ff8848bc3511e4b335ac3972615ffab1c0f6
Guxxtavoww <gustavoaugustocar@hotmail.com>
2026-07-31 16:07:40 -0300

    Chore: adding init in cache module

--- gorm/utils/unrwrap-error.go
+++ gorm/utils/unrwrap-error.go
@@ -1,8 +1,22 @@
 package utils
 
+import "fmt"
+
 func UnrwrapError[T any](value T, err error) T {
 	if err != nil {
-		panic("Error")
+		panic(err)
+	}
+
+	return value
+}
+
+func Must[T any](value T, err error, msgAndArgs ...any) T {
+	if err != nil {
+		if len(msgAndArgs) > 0 {
+			panic(fmt.Sprintf("%v: %v", fmt.Sprintf(msgAndArgs[0].(string), msgAndArgs[1:]...), err))
+		}
+
+		panic(err)
 	}
 
 	return value
